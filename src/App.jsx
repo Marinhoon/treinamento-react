143824fd57f1b979450827f4ceb5620e89b35cca
Vitor Marinho <vitormarinhoh@gmail.com>
2024-04-03 17:07:49 -0300

    INICIO

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { Post } from './Post'
+
+
+export function App() {
+
+  return (
+    <div>
+      
+    <Post 
+      author="Vitor Marinho"
+      content="Marido"
+    />
+    <Post
+      author="Franciele"
+      content="Esposa"
+    />
+    </div>
+  )
+}
+
